c239fb804a151920f923058a3f606920c01aa948
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2023-11-05 18:58:58 +0200

    create notes list view

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
+import 'package:note_app/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -10,59 +10,15 @@ class NotesViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(vertical: 24, horizontal: 24),
       child: Column(
-        children: [CustomAppBar(), NoteItem()],
-      ),
-    );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-          color: const Color(0xffE3C25F),
-          borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                'Build your career with Tharwat Samy',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(.5),
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
-              ),
-            ),
+          CustomAppBar(),
+          Expanded(
+            child: NotesListView(),
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24, top: 16),
-            child: Text(
-              'May 21.2022',
-              style:
-                  TextStyle(color: Colors.black.withOpacity(.5), fontSize: 14),
-            ),
-          )
         ],
       ),
     );
   }
 }
+
+

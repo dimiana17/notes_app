a1da93b6882c846bbd0eb2d69d6ced674eed8c93
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2023-11-06 15:27:33 +0200

    add notes state

--- lib/cubits/cubit/add_note_cubit_state.dart
+++ lib/cubits/cubit/add_note_cubit_state.dart
@@ -1,6 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'add_note_cubit_cubit.dart';
 
 @immutable
-sealed class AddNoteCubitState {}
+abstract class AddNoteCubitState {}
 
-final class AddNoteCubitInitial extends AddNoteCubitState {}
+class AddNoteCubitInitial extends AddNoteCubitState {}
+class AddNoteLoading extends AddNoteCubitState {}
+class AddNoteSuccess extends AddNoteCubitState {}
+class AddNoteFailure extends AddNoteCubitState {
+  final String errMessage;
+  AddNoteFailure(
+    this.errMessage,
+  );
+}
